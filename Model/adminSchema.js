17b8c26efa1e71af67933d9eac5240b58b25b2b3
shemil01 <shemilmk1033@gmail.com>
2024-05-31 11:02:51 +0530

    updated

--- Model/adminSchema.js
+++ Model/adminSchema.js
@@ -1,26 +1,20 @@
-const mongoose = require('mongoose')
-
-
-
+const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
-
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    
-    username:{
-        type:String,
-        unique:true
-    },
-    
-    password:{
+    email: {
         type: String,
-        required:true,
-        unique:true
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
     }
-})
+});
 
-module.exports=mongoose.model("Admin",adminSchema)
+module.exports = mongoose.model("Admin", adminSchema);
