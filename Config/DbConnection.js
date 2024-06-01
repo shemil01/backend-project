9933cea9783ee3ecfc500cb3fc952f88e2b9b7c3
shemil01 <shemilmk1033@gmail.com>
2024-06-01 16:42:02 +0530

    admin updated

--- Config/DbConnection.js
+++ Config/DbConnection.js
@@ -1,17 +1,14 @@
-const mongoos = require('mongoose')
+const mongoos = require("mongoose");
 
-
-const connectDb=async function(){
-try{
-
-    mongoos.connect("mongodb+srv://shemil:shemil01@atlascluster.8ivbdk1.mongodb.net/PLASHOE")
-    .then(()=>{
-        console.log("Data base connected");
-    })
-}catch(error){
+const connectDb = async function () {
+  try {
+    await mongoos.connect(process.env.mongoDB_atlas)
+    .then(() => {
+      console.log("Data base connected");
+    });
+  } catch (error) {
     console.log(error);
-    process.exit(1)
-}
-    
-}
-module.exports = connectDb    
+    process.exit(1);
+  }
+};
+module.exports = connectDb;
